d62b93163ecf06adbe0e5fa994715d53c066b6b0
Nathan McKinley <ndm25@case.edu>
2012-03-20 13:44:33 -0400

    first mckinley level done
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

---
+++ easy_problems/ndm_1/printingdates.c
@@ -0,0 +1,9 @@
+// This application prints the current date by asking the kernel to print the date.
+// Your goal is to read password.txt, which is owned by the user who wrote this program.
+// How can you do that?
+
+int main(int argc, char** argv)
+{
+    system("date");
+    return 0;
+}
