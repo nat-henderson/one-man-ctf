5cca73dd68346d244450c8739965d7bff729cd3e
Nathan McKinley <ndm25@case.edu>
2012-03-22 13:29:55 -0400

    working server/client
    Signed-off-by: Nathan McKinley <ndm25@case.edu>

---
+++ hard_problems/ndm_2/vulnerable_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Echo client program
+import socket
+import sys
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    while (True):
+        s.send('somethinggoeshere')
+        data = s.recv(1024)
+        print 'Received', repr(data)
+
+def print_usage():
+    sys.stderr.write("Usage:  " + sys.argv[0] + " hostname [port]\n")
+
+if __name__ == '__main__':
+    argc = len(sys.argv)
+    if argc == 1:
+        print_usage()
+        quit()
+    elif argc >= 2:
+        host = sys.argv[1]
+        port = 9125 
+    elif argc >= 3:
+        host = sys.argv[1]
+        port = sys.argv[2]
+    main()
+    s.close()
